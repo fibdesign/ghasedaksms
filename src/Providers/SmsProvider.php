12e55950fc53fbb304e0f9abd7d4c8c6014e5f59
Sadegh <74778568+msghaderi77@users.noreply.github.com>
2023-10-22 21:47:15 +0330

    Update SmsProvider.php

--- src/Providers/SmsProvider.php
+++ src/Providers/SmsProvider.php
@@ -5,10 +5,33 @@ use Illuminate\Support\ServiceProvider;
 
 class SmsProvider extends ServiceProvider
 {
+    public function register()
+    {
+        $this->mergeConfig();
+    }
+
     public function boot()
     {
-        $this->publishes([
-            __DIR__.'/../config/sms.php' => config_path('sms.php'),
-        ]);
+        $this->publishConfig();
     }
+
+    private function mergeConfig()
+    {
+        $path = $this->getConfigPath();
+        $this->mergeConfigFrom($path, 'bar');
+    }
+
+    private function publishConfig()
+    {
+        $path = $this->getConfigPath();
+        $this->publishes([$path => config_path('sms.php')], 'config');
+    }
+
+    private function getConfigPath(): string
+    {
+        return __DIR__ . '/../config/sms.php';
+    }
+
+
+
 }
